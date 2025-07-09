412577a82e246f6ed4fd23c64a004550ce9a33e1
Matheus Sandi <Matheussandi@hotmail.com>
2025-07-09 12:16:02 -0300

    feat: add drizzle and seed

--- server/src/db/connection.ts
+++ server/src/db/connection.ts
@@ -1,4 +1,9 @@
 import postgres from "postgres";
+import { drizzle } from "drizzle-orm/postgres-js";
 import { env } from "../env.ts";
 
 export const sql = postgres(env.DATABASE_URL);
+export const db = drizzle(sql, {
+  schema: {},
+  casing: "snake_case",
+});
