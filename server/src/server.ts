e83d31c5f4aca71936df279e8efb9f6e625db071
Matheus Sandi <Matheussandi@hotmail.com>
2025-07-09 15:10:14 -0300

    fix(backend): adjust url cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,7 +13,7 @@ import { getRooms } from "../http/routes/get-rooms.ts";
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
-  origin: "https:localhost:5173",
+  origin: "http://localhost:5173",
 });
 
 app.setValidatorCompiler(validatorCompiler);
