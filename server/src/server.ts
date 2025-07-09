24c072699e6fdff859ab90cf8774ac52ae059b52
Matheus Sandi <Matheussandi@hotmail.com>
2025-07-09 11:20:51 -0300

    feat: add biomejs

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,19 +12,22 @@ import { env } from "./env.ts";
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
-    origin: 'https:localhost:5173',
+  origin: "https:localhost:5173",
 });
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-app.get('/health', () => {
-  return 'OK';
+app.get("/health", () => {
+  return "OK";
 });
 
-app.listen({ port: env.PORT }).then(() => {
-  console.log(`Server is running on port ${env.PORT}`);
-}).catch((err) => {
-  console.error("Error starting server:", err);
-  process.exit(1);
-});
+app
+  .listen({ port: env.PORT })
+  .then(() => {
+    console.log(`Server is running on port ${env.PORT}`);
+  })
+  .catch((err) => {
+    console.error("Error starting server:", err);
+    process.exit(1);
+  });
