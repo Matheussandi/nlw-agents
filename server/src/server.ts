681f8ed3ca301d931e8c90a9d89cb356b06c297d
Matheus Sandi <Matheussandi@hotmail.com>
2025-07-12 14:41:20 -0300

    feat(backend): add POST rooms

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,6 +9,7 @@ import {
 } from "fastify-type-provider-zod";
 
 import { getRoomsRoute } from "./http/routes/get-rooms.ts";
+import { createRoomRoute } from "./http/routes/create-room.ts";
 
 import { env } from "./env.ts";
 
@@ -26,6 +27,7 @@ app.get("/health", () => {
 });
 
 app.register(getRoomsRoute);
+app.register(createRoomRoute);
 
 app
   .listen({ port: env.PORT })
