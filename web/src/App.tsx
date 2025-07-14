bb7718fc389abade31f3072c4d713cdf89ca5f71
Matheus Sandi <Matheussandi@hotmail.com>
2025-07-14 20:46:44 -0300

    feat: organize routes and add question and room components

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { CreateRoom } from "./pages/CreateRoom";
-import { RoomDetails } from "./pages/RoomDetails";
 import { Toaster } from "./components/ui/sonner";
+import { CreateRoom } from "./pages/create-room";
+import { Room } from "./pages/room";
 
 const queryClient = new QueryClient()
 
@@ -11,8 +11,9 @@ export default function App() {
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Routes>
-          <Route element={<CreateRoom />} index />
-          <Route element={<RoomDetails />} path="/room/:roomId" />
+          <Route element={<Room />} index />
+          <Route element={<CreateRoom />} path="/create-room" />
+          <Route element={<Room />} path="/room/:roomId" />
         </Routes>
         <Toaster />
       </BrowserRouter>
