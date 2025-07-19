899a5c7fb010b00a8a76991577d81b068ab8e6f1
Matheus Sandi <Matheussandi@hotmail.com>
2025-07-19 16:24:23 -0300

    feat: add integration with Google GenAI for audio transcription and embedding generation

--- server/src/env.ts
+++ server/src/env.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
   DATABASE_URL: z.string().url().startsWith("postgres://"),
+  AI_API_KEY: z.string().min(1, "AI_API_KEY is required"),
 });
 
 export const env = envSchema.parse(process.env);
--- server/src/http/routes/upload-audio.ts
+++ server/src/http/routes/upload-audio.ts
@@ -2,6 +2,7 @@ import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod";
 import { z } from "zod/v4";
 import { db } from "../../db/connection.ts";
 import { schema } from "../../db/schema/index.ts";
+import { generateEmbedding, transcribeAudio } from "../../services/gemini.ts";
 
 export const uploadAudioRoute: FastifyPluginCallbackZod = (app) => {
   app.post(
@@ -16,11 +17,38 @@ export const uploadAudioRoute: FastifyPluginCallbackZod = (app) => {
     async (request, reply) => {
       const { roomId } = request.params;
 
-      const audio = request.file();
+      const audio = await request.file();
 
       if (!audio) {
         throw new Error("Audio é obrigatório");
       }
+
+      const audioBuffer = await audio.toBuffer();
+      const audioAsBase64 = audioBuffer.toString("base64");
+
+      const transcription = await transcribeAudio(
+        audioAsBase64,
+        audio.mimetype
+      );
+
+      const embeddings = await generateEmbedding(transcription);
+
+      const result = await db
+        .insert(schema.audioChunks)
+        .values({
+          roomId,
+          transcription,
+          embeddings,
+        })
+        .returning();
+
+      const chunk = result[0];
+
+      if (!chunk) {
+        throw new Error("Erro ao salvar chunk de aúdio.");
+      }
+
+      return reply.status(201).send({ chunkId: chunk.id });
     }
   );
 };
